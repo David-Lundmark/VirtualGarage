f3677da19455e3a70c4ac5904c2542dd9b3094f2
David-Lundmark <david.lundmarkbredband.net>
2016-10-25 12:53:52 +0200

    Basic classes

---
+++ VirtualGarage/Garage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualGarage
+{
+    class Garage<T> : IEnumerable<T> where T : Vehicle
+    {
+        uint capacity;
+
+        public Garage(uint limit = uint.MaxValue)
+        {
+            capacity = limit;
+        }
+    }
+}
---
+++ VirtualGarage/Vehicle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualGarage
+{
+    class Vehicle
+    {
+        int registrationNumber;
+        string color;
+        int numWheels;
+    }
+
+    class Airplane : Vehicle
+    {
+
+    }
+
+    class Motorcycle : Vehicle
+    {
+
+    }
+
+    class Car : Vehicle
+    {
+
+    }
+    
+    class Bus : Vehicle
+    {
+
+    }
+    
+    class Boat : Vehicle
+    {
+
+    }
+}
